9eb1792daae55bf1ddf2b74f5b3ef9d7c7631442
glebgol <glebgolovnev2004@gmail.com>
2025-02-03 21:55:42 +0300

    Refactored TagRepository to work with rxjava

--- app/src/main/java/com/glebgol/photospots/data/DefaultTagRepository.kt
+++ app/src/main/java/com/glebgol/photospots/data/DefaultTagRepository.kt
@@ -1,26 +1,73 @@
 package com.glebgol.photospots.data
 
+import com.glebgol.photospots.domain.client.ApiClient
 import com.glebgol.photospots.domain.data.Tag
 import com.glebgol.photospots.domain.data.TagDetails
 import com.glebgol.photospots.domain.repository.TagRepository
+import io.reactivex.rxjava3.core.Single
+import okhttp3.MediaType
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 class DefaultTagRepository : TagRepository {
 
-    override suspend fun getTags(): List<Tag> {
-        TODO("Not yet implemented")
+    override fun getTags(): Single<List<Tag>> {
+        return Single.create { emitter ->
+            try {
+                val response = ApiClient.tagApi.getTags().execute()
+                if (response.isSuccessful) {
+                    emitter.onSuccess(response.body() ?: emptyList())
+                } else {
+                    emitter.onError(Exception("Error: ${response.code()}"))
+                }
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }
     }
 
-    override suspend fun getTagById(id: Long): TagDetails {
-        TODO("Not yet implemented")
+    override fun getTagById(id: Long): Single<TagDetails> {
+        return Single.create { emitter ->
+            try {
+                val response = ApiClient.tagApi.getTagById(id).execute()
+                if (response.isSuccessful) {
+                    emitter.onSuccess(response.body()!!)
+                } else {
+                    emitter.onError(
+                        Exception("Error while getting tag details: ${response.code()}")
+                    )
+                }
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }
     }
 
-    override suspend fun createTag(
-        image: MultipartBody.Part,
-        description: String,
-        longitude: Double,
-        latitude: Double
-    ): TagDetails {
-        TODO("Not yet implemented")
+    override fun createTag(image: File, description: String, longitude: Double, latitude: Double)
+    : Single<TagDetails> {
+        return Single.create { emitter ->
+            try {
+                val response = ApiClient.tagApi.createTag(image = getImagePart(image),
+                    description = description, longitude = longitude, latitude = latitude).execute()
+                if (response.isSuccessful) {
+                    emitter.onSuccess(response.body()!!)
+                } else {
+                    emitter.onError(
+                        Exception("Error while creating tag ${response.code()}")
+                    )
+                }
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }
+    }
+
+    companion object {
+        private fun getImagePart(image: File): MultipartBody.Part {
+            val requestFile = RequestBody.create(MediaType.parse("image/jpeg"), image)
+            val imagePart = MultipartBody.Part.createFormData("image", image.name, requestFile)
+            return imagePart
+        }
     }
 }
--- app/src/main/java/com/glebgol/photospots/domain/repository/TagRepository.kt
+++ app/src/main/java/com/glebgol/photospots/domain/repository/TagRepository.kt
@@ -2,15 +2,17 @@ package com.glebgol.photospots.domain.repository
 
 import com.glebgol.photospots.domain.data.Tag
 import com.glebgol.photospots.domain.data.TagDetails
+import io.reactivex.rxjava3.core.Single
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Path
+import java.io.File
 
 interface TagRepository {
 
-    suspend fun getTags(): List<Tag>
-    suspend fun getTagById(id: Long): TagDetails
-    suspend fun createTag(
-        image: MultipartBody.Part, description: String, longitude: Double, latitude: Double
-    ): TagDetails
+    fun getTags(): Single<List<Tag>>
+    fun getTagById(id: Long): Single<TagDetails>
+    fun createTag(
+        image: File, description: String, longitude: Double, latitude: Double
+    ): Single<TagDetails>
 }
