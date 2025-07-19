63606fbdfd2935d9ed8d6c1ec0002a1e8fd73e6c
glebgol <glebgolovnev2004@gmail.com>
2025-07-20 00:24:39 +0300

    Migration 2-3: added indices for 'isFavourite' and 'id' columns

--- app/src/main/kotlin/com/glebgol/photospots/data/room/dao/TagEntity.kt
+++ app/src/main/kotlin/com/glebgol/photospots/data/room/dao/TagEntity.kt
@@ -2,10 +2,17 @@ package com.glebgol.photospots.data.room.dao
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.glebgol.photospots.data.room.dao.TagEntity.Companion.TABLE_NAME
 
-@Entity(tableName = TABLE_NAME)
+@Entity(
+    tableName = TABLE_NAME,
+    indices = [
+        Index(value = ["isFavourite"]),
+        Index(value = ["id"])
+    ]
+)
 data class TagEntity(
     @PrimaryKey
     val id: String,
